63e3dcdb62633ea954ce6d7d267424813429ae83
Сыркина Елизавета Тимофеевна <ESyrkina@alfabank.ru>
2025-09-12 21:01:17 +0300

    homework lesson04

---
+++ src/main/kotlin/lessons/lessons04/Datatypes.kt
@@ -0,0 +1,53 @@
+package org.example.lessons.lessons04
+
+import java.lang.Boolean.FALSE
+import java.lang.Boolean.TRUE
+
+val v1: Int = 42
+val v2: Long = 98765432123456789L
+val v3: Float = 23.45f
+val v4: Double = 0.123456789
+val v5: String = "Kotlin & Java"
+val v6: Boolean = FALSE
+val v7: Char = 'c'
+val v8: Short = 500
+val v9: Long = 4294967296L
+val v10: Float = 18.0f
+val v11: Double = -0.001
+val v12: String = "OpenAI"
+val v13: String = "true"
+val v14: List<Int> = listOf(3, 14)
+val v15:Char = '9'
+val v16:Int = 2048
+val v17:Long = 10000000000L
+val v18:Set<String> = setOf("OpenAI", "Quantum Computing")
+val v19:Float = 5.75f
+//Некорректные кавычки
+// val v20:Any = `1.414`
+val v21:String = "Artificial Intelligence"
+//В массиве должны быть одинаковые типы
+// val v22:Array<Char> = arrayOf('x', "A")
+val v23:String = "Android Studio"
+val v24:Char = '@'
+val v25:Short = 1024
+val v26:Long = 1234567890123L
+val v27:Float = 10.01f
+val v28:Double = -273.15
+val v29:String = "SpaceX"
+val v30:Boolean = FALSE
+val v31:Double = 0.007
+//Некорректные кавычки
+// val v32:Char = “🤯”
+val v33:Map<String,Int> = mapOf("true" to 2, "false" to 34)
+//Некорректные кавычки
+//val v34:Any =‘65535’
+val v35:Long = 72057594037927935L
+val v36:Float = 2.71828f
+val v37:Double = 101.0101
+val v38:String = "Quantum Computing"
+val v39:Map<Int,String> = mapOf(2 to "true", 34 to "false")
+val v40:Char = 'x'
+val v41:Int = 314
+val v42:Long = 123456789123456789L
+val v43:Float = 6.626f
+val v44:Boolean = TRUE
